cd22d0002e3073999fa478d406ca17dd3a1bebce
Ludensburger <147230279+Ludensburger@users.noreply.github.com>
2024-11-10 18:44:06 +0800

    Add axios and MUI icons, implement new releases feature in App component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,19 +1,68 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
-import { Box } from "@mui/system";
-import { Container } from "@mui/material";
+import { Box, Grid } from "@mui/system";
+import {
+  Container,
+  Divider,
+  Paper,
+  TextField,
+  Typography,
+} from "@mui/material";
+import MusicCard from "./components/MusicCard";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [newReleases, setNewReleases] = useState([]);
+
+  useEffect(() => {
+    const fetchNewReleases = async () => {
+      try {
+        const response = await axios.get(
+          "http://localhost:4000/api/tracks/new-releases"
+        );
+        setNewReleases(response.data.albums.items);
+      } catch (error) {
+        console.error("Could not fetch new releases", error);
+      }
+    };
+
+    fetchNewReleases();
+  }, []);
+
+  console.log(newReleases);
 
   return (
     <>
       <Container>
-        <Box>
-          <h1 className="text-3xl font-bold underline">TOBOL</h1>
-        </Box>
+        <Paper>
+          <Box component="section" className="mb-10 mx-5">
+            <Typography variant="h4" component="h1" className="!font-bold">
+              Octave App by Ryuken
+            </Typography>
+          </Box>
+          <Box component="section" className="my-5 mx-5">
+            <TextField
+              id="outlined-basic"
+              label="Search"
+              variant="outlined"
+              className="w-full"
+            />
+          </Box>
+          <Divider />
+          <Box component="section" className="my-5 mx-5">
+            <Grid container spacing={2} columns={2}>
+              {newReleases.map((release) => (
+                <Grid item key={release.id} size={1}>
+                  <MusicCard
+                    title={release.name}
+                    artist={release.artists[0].name}
+                    image={release.images[0].url}
+                  />
+                </Grid>
+              ))}
+            </Grid>
+          </Box>
+        </Paper>
       </Container>
     </>
   );
---
+++ frontend/src/components/MusicCard.jsx
@@ -0,0 +1,43 @@
+import {
+  Card,
+  CardContent,
+  CardMedia,
+  IconButton,
+  Box,
+  Typography,
+} from "@mui/material";
+import PlayArrowIcon from "@mui/icons-material/PlayArrow";
+
+function MusicCard({ title, artist, image }) {
+  return (
+    <Card sx={{ display: "flex" }}>
+      <Box sx={{ display: "flex", flexDirection: "column" }}>
+        <CardContent sx={{ flex: "1 0 auto" }}>
+          <Typography component="div" variant="h5">
+            {title}
+          </Typography>
+          <Typography
+            variant="subtitle1"
+            component="div"
+            sx={{ color: "text.secondary" }}
+          >
+            {artist}
+          </Typography>
+        </CardContent>
+        <Box sx={{ display: "flex", alignItems: "center", pl: 1, pb: 1 }}>
+          <IconButton aria-label="play/pause">
+            <PlayArrowIcon sx={{ height: 38, width: 38 }} />
+          </IconButton>
+        </Box>
+      </Box>
+      <CardMedia
+        component="img"
+        sx={{ width: 151 }}
+        image={image}
+        alt="Live from space album cover"
+      />
+    </Card>
+  );
+}
+
+export default MusicCard;
