3e85b003fb631ba066b0f16169b7f1a5f3f038b3
Ludensburger <147230279+Ludensburger@users.noreply.github.com>
2024-11-10 18:44:35 +0800

    Add tracks route to fetch new releases from Spotify API

---
+++ backend/routes/tracks.js
@@ -0,0 +1,28 @@
+const { spotifyApi } = require("../utils/spotify");
+const express = require("express");
+
+const router = express.Router();
+
+router.get("/new-releases", (req, res, next) => {
+  const fetchNewReleases = () => {
+    spotifyApi
+      .getNewReleases()
+      .then((data) => {
+        res.json(data.body);
+      })
+      .catch((err) => {
+        if (req.retry) {
+          // If the request was retried and still failed, send the error response
+          res
+            .status(err.body.error.status)
+            .json({ error: err.body.error.message });
+        } else {
+          next(err);
+        }
+      });
+  };
+
+  fetchNewReleases();
+});
+
+module.exports = router;
