9a0558404aabcff6c0d9acd7a9ad8e585b57528c
Ludensburger <147230279+Ludensburger@users.noreply.github.com>
2024-11-11 10:23:33 +0800

    Enhance MusicCard component to include external link and improve layout responsiveness

--- frontend/src/components/MusicCard.jsx
+++ frontend/src/components/MusicCard.jsx
@@ -8,24 +8,41 @@ import {
 } from "@mui/material";
 import PlayArrowIcon from "@mui/icons-material/PlayArrow";
 
-function MusicCard({ title, artist, image }) {
+function MusicCard({ title, artist, image, link }) {
   return (
-    <Card sx={{ display: "flex", justifyContent: "space-between" }}>
-      <Box sx={{ display: "flex", flexDirection: "column" }}>
+    <Card
+      sx={{ display: "flex", justifyContent: "space-between", height: "100%" }}
+    >
+      <Box sx={{ display: "flex", flexDirection: "column", maxWidth: "70%" }}>
         <CardContent sx={{ flex: "1 0 auto" }}>
-          <Typography component="div" variant="h5">
+          <Typography
+            component="div"
+            variant="h5"
+            sx={{
+              textAlign: "left",
+              whiteSpace: "nowrap",
+              overflow: "hidden",
+              textOverflow: "ellipsis",
+            }}
+          >
             {title}
           </Typography>
           <Typography
             variant="subtitle1"
             component="div"
-            sx={{ color: "text.secondary" }}
+            sx={{ color: "text.secondary", textAlign: "left" }}
           >
             {artist}
           </Typography>
         </CardContent>
         <Box sx={{ display: "flex", alignItems: "center", pl: 1, pb: 1 }}>
-          <IconButton aria-label="play/pause">
+          <IconButton
+            aria-label="play/pause"
+            component="a"
+            href={link}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
             <PlayArrowIcon sx={{ height: 38, width: 38 }} />
           </IconButton>
         </Box>
@@ -34,8 +51,7 @@ function MusicCard({ title, artist, image }) {
         component="img"
         sx={{
           width: "30%",
-          height: "30%",
-          alignItems: "center",
+          objectFit: "cover",
         }}
         image={image}
         alt="Live from space album cover"
