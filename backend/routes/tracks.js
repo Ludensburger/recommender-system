1d75c381e0e464cb17edd2c2f3d51323d34ebbf9
Ludensburger <mendozaryu08@gmail.com>
2024-11-15 01:16:44 +0800

    Refactor new releases endpoint to return album items and add recommendations endpoint

--- backend/routes/tracks.js
+++ backend/routes/tracks.js
@@ -8,12 +8,12 @@ router.get("/new-releases", (req, res, next) => {
     spotifyApi
       .getNewReleases()
       .then((data) => {
-        const newReleases = data.body;
+        const newReleases = data.body.albums.items;
         res.json(newReleases);
       })
       .catch((err) => {
+        console.error("Error fetching new releases:", err);
         if (req.retry) {
-          // If the request was retried and still failed, send the error response
           res
             .status(err.body.error.status)
             .json({ error: err.body.error.message });
@@ -24,6 +24,34 @@ router.get("/new-releases", (req, res, next) => {
   };
 
   fetchNewReleases();
+});
+
+router.get("/get-recommendations", (req, res, next) => {
+  const fetchRecommendations = () => {
+    spotifyApi
+      .getRecommendations({
+        seed_artists: ["4NHQUGzhtTLFvgF5SZesLK"],
+        min_energy: 0.4,
+        min_popularity: 50,
+      })
+      .then((data) => {
+        const recommendations = data.body.tracks;
+        console.log("Fetched recommendations:", recommendations);
+        res.json(recommendations);
+      })
+      .catch((err) => {
+        console.error("Error fetching recommendations:", err);
+        if (req.retry) {
+          res
+            .status(err.body.error.status)
+            .json({ error: err.body.error.message });
+        } else {
+          next(err);
+        }
+      });
+  };
+
+  fetchRecommendations();
 });
 
 module.exports = router;
